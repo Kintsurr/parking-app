c0fac5e0c1c5f9c5bbcdfba16d9cc3ac6051c55f
Giorgi Kintsurashvili <giokintsurashvili0@gmail.com>
2023-09-28 20:16:55 +0400

    deleted some temporary files, nothing else is changed

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(express.json()); // parse json bodies in the request object
 
 // Redirect requests to endpoint starting with /posts to postRoutes.js
 app.use("/", require('./routes/authRoute'))
-app.use("/posts", require("./routes/postRoutes"))
 app.use('/users',verifyToken.verifyToken, userRoutes);
 
 
